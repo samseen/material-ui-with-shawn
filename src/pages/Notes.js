7cb9bd62b1094a4dd57a2824da5ebf8971059ec7
Samson Akanbi <samson.akanbi@outlook.com>
2021-03-29 10:43:55 +0100

    Handled the deletion of items on the card

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -13,13 +13,23 @@ export default function Notes() {
       .then(data => setNotes(data))
   }, [])
 
+  const handleDelete = async (id) => {
+    await fetch('http://localhost:8000/notes/' + id, {
+      method: 'DELETE'
+    })
+
+    const newNotes = notes.filter(note => note.id != id)
+
+    setNotes(newNotes)
+  }
+
   return (
     <Container>
 
       <Grid container spacing={3}>
         {notes.map(note => (
           <Grid item key={note.id} xs={12} md={6} lg={4}>
-            <NoteCard note={note} />
+            <NoteCard note={note} handleDelete={handleDelete} />
           </Grid>
         ))}
       </Grid>
