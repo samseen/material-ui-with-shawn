2b5f749496e2ea6e4c5c7b35e684bfd07796d155
Samson Akanbi <samson.akanbi@outlook.com>
2021-03-29 10:17:49 +0100

    Grid Component

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import { Container } from '@material-ui/core'
 
 export default function Notes() {
   const [notes, setNotes] = useState([])
@@ -10,10 +13,17 @@ export default function Notes() {
   }, [])
 
   return (
-    <div>
-      {notes.map(note => (
-        <p key={note.id}>{ note.title }</p>
-      ))}
-    </div>
+    <Container>
+
+      <Grid container>
+        {notes.map(note => (
+          <Grid item key={note.id} xs={12} md={6} lg={4}>
+            <Paper>{ note.title }</Paper>
+          </Grid>
+        ))}
+      </Grid>
+
+      
+    </Container>
   )
 }
