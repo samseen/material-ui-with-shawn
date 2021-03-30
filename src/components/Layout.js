c8c92097dcdd7c122fbac9baf53c65f77c10805d
Samson Akanbi <samson.akanbi@outlook.com>
2021-03-30 07:26:53 +0100

    Permanent Drawer

---
+++ src/components/Layout.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core'
+import Drawer from '@material-ui/core/Drawer'
+import Typography from '@material-ui/core/Typography'
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles({
+    page: {
+        background: '#f9f9f9',
+        width: '100%'
+    },
+    drawer: {
+        width: drawerWidth
+    },
+    drawerPaper: {
+        width: drawerWidth
+    },
+    root: {
+        display: 'flex'
+    }
+})
+
+const Layout = ({ children }) => {
+    const classes = useStyles()
+
+    return (
+        <div className={classes.root}>
+            {/* app bar */}
+
+            {/* side bar */}
+            <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                anchor="left"
+                classes={{ paper: classes.drawerPaper }}
+            >
+                <div>
+                    <Typography variant="h5">
+                        Ninja Notes
+                    </Typography>
+                </div>
+            </Drawer>
+            
+            <div className={classes.page}>
+                {children}
+            </div>
+        </div>
+    )
+}
+
+export default Layout
